ea9ea8d163e35f302479e42b6dcee03a72a1af34
Danh <danh.nguyen@appscyclone.com>
2023-07-05 15:52:50 +0700

    #< SC > #Init authentication and base tables

---
+++ app/Http/Controllers/ProductApiController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductModel;
+use Illuminate\Http\Request;
+
+class ProductApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductModel $productModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProductModel $productModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductModel $productModel)
+    {
+        //
+    }
+}
